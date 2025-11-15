4d2e3876ea60b4c02b93958192eb3044f64990cf
Varun2055 <151852220+Varun2055@users.noreply.github.com>
2025-11-15 16:13:58 +0530

    Add additional base URL to API configuration

--- movie-ticket-booking/frontend/src/api/api.js
+++ movie-ticket-booking/frontend/src/api/api.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const API = axios.create({
     baseURL: "http://52.200.176.69:8000",
     "http://127.0.0.0:8000",
+    "http://13.222.219.142:8000",
 });
 
 API.interceptors.request.use((req) => {
